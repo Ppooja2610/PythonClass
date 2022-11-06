061393e689e89bbc29df89cd7d3f6d5c532b341f
Ppooja2610 <117577824+Ppooja2610@users.noreply.github.com>
2022-11-06 18:29:53 +0530

    Create python_decisionmaking.py

---
+++ python_decisionmaking.py
@@ -0,0 +1,17 @@
+
+x = 10
+y = 5
+
+if x==y:
+    print("Yes, X is equal to Y")
+else:
+     print("No they are not equal")   
+
+marks = 10
+
+if marks >= 90:
+    print("A+ Grade")
+elif marks>=80 and marks<=90:
+    print("A Grade")
+else:
+    print("Fail")
