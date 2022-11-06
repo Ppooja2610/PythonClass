4cfc0d4cc8925c81a7410b70df027ef2677d13a9
Ppooja2610 <117577824+Ppooja2610@users.noreply.github.com>
2022-11-06 18:29:18 +0530

    Create python_operators.py

---
+++ python_operators.py
@@ -0,0 +1,36 @@
+# Numeric operators in Python
+# + for addition
+# - for substract
+# * for multipply
+# / for float division
+# // for int division
+# ** for Power Calculation
+# % for Modulus
+
+x = 5
+y = 3
+
+print("Addition of x  + y =",x+y)
+print("Subtract of x  - y =",x-y)
+print("Multiply of x * y =",x*y)
+print("Float Division of x / y =",x/y)
+print("Int Division of x // y =",x//y)
+print("Power of x ** y =",x**y)
+print("Modulus of x % y =",x%y)
+
+# Some operations on String 
+
+str_data = "Pooja"
+empty_str = ''
+
+#Concat
+
+full_name = str_data + " " + "Gaikwad"
+print("Full name is : ",full_name)
+
+# Repeat String multiple times
+repeat_name = "Pooja "*4
+print("Repeat name is : ",repeat_name) 
+
+
+
