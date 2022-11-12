f84bfeaea9225928969a699e40e40c24bdd6888d
Ppooja2610 <117577824+Ppooja2610@users.noreply.github.com>
2022-11-12 19:00:54 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -36,9 +36,25 @@ elif(num2>num1 and num2>num3):
 else:
     print("Greatest Number is :",num3)
 
+
+#Q23. Write a code that displays the sum of all the even numbers from the given list.
+numbers = [12, 75, 150, 180, 145, 525, 50]
+sum_result = 0
+for num in numbers:
+    if(num %2 == 0):
+        sum_result = sum_result + num
+print('Total Sum is :', sum_result)
+ 
+
 #Q25. Write a program to display only those numbers from a list that satisfy the following conditions
 # The number must be divisible by five
 # If the number is greater than 150, then skip it and move to the next number
 #If the number is greater than 500, then stop the loop
-#numbers = [12, 75, 150, 180, 145, 525, 50]    
+numbers = [12, 75, 150, 180, 145, 525, 50]  
+for num in numbers:
+    if (num%5 == 0):
+        if(num > 150):
+            continue
+    elif(num > 500):
+    break 
 
