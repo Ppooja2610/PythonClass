a14dc904d65f9b14c586d4238b64ac83bbbf27c4
Ppooja2610 <117577824+Ppooja2610@users.noreply.github.com>
2022-11-22 16:38:44 +0530

    Update MegaAssignment.py

--- MegaAssignment.py
+++ MegaAssignment.py
@@ -222,3 +222,13 @@ for i in range(0, rows):
     for j in range(0, i + 1):  
         print("* ", end="")  # printing star  
     print("") 
+
+Q99 Pattern  
+rows = int(input("Enter the number of rows: "))  
+  
+# This will print the rows  
+for i in range(1, rows+1):  
+    # This will print number of column  
+    for j in range(1, i + 1):  
+        print(j, end=' ')  
+    print("")  
